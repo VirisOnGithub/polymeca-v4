be85b0b4d4108d8fb0f1655351bb5433ab0872d9
Clément RENIERS <clement.reniers@etu.univ-lyon1.fr>
2026-07-19 17:09:42 +0200

    feat: footer

---
+++ app/components/FooterCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  icon: String,
+  title: String,
+  content: {
+    type: [String, Array],
+    required: true
+  },
+  onclick: Function
+})
+</script>
+
+<template>
+  <div>
+    <p class="text-muted">
+      {{ props.title.toUpperCase() }}
+    </p>
+    <div class="flex">
+      <UIcon
+        :name="props.icon"
+        class="bg-secondary"
+      />
+      <p v-if="typeof(props.content) === 'string'">
+        {{ props.content }}
+      </p>
+      <div
+        v-for="line in props.content"
+        v-else
+        :key="line"
+        class="inline"
+      >
+        <p>{{ line }}</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ app/pages/team.vue
@@ -0,0 +1,3 @@
+<template>
+  <TeamPresentation />
+</template>
