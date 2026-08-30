1611b3b44f4a60d3c140f1dff59f75e7dc38df84
VirisOnGithub <clement.reniers00@gmail.com>
2026-08-30 23:17:27 +0200

    fix: remove dummy articles

--- app/composables/useArticles.ts
+++ app/composables/useArticles.ts
@@ -5,14 +5,14 @@ const articles = [
     description: 'L\'équipe a participé pour la toute permière fois à la Formula Student. Si les premiers tests ont été concluants (tech, tilt, noise, brake), nous avons eu des problèmes avec l\'accélération, le skidpad et l\'endurance. Nous reviendrons l\'année prochaine pour donner encore plus de nous.',
     additional_images: ['/articles/001.jpg', '/articles/002.jpg'],
     date: new Date()
-  },
-  {
-    splash: '/car_fs.jpg',
-    title: 'Participation à la Formula Student',
-    description: 'L\'équipe a participé pour la toute permière fois à la Formula Student. Si les premiers tests ont été concluants (tech, tilt, noise, brake), nous avons eu des problèmes avec l\'accélération, le skidpad et l\'endurance. Nous reviendrons l\'année prochaine pour donner encore plus de nous.',
-    additional_images: ['/articles/001.jpg', '/articles/002.jpg'],
-    date: new Date()
   }
+  // {
+  //   splash: '/car_fs.jpg',
+  //   title: 'Participation à la Formula Student',
+  //   description: 'L\'équipe a participé pour la toute permière fois à la Formula Student. Si les premiers tests ont été concluants (tech, tilt, noise, brake), nous avons eu des problèmes avec l\'accélération, le skidpad et l\'endurance. Nous reviendrons l\'année prochaine pour donner encore plus de nous.',
+  //   additional_images: ['/articles/001.jpg', '/articles/002.jpg'],
+  //   date: new Date()
+  // }
 ]
 
 export const latestArticle = () => {
